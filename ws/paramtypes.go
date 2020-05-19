c425c0314e7a119f5116a366f919dd67c94b4891
Bob620 <bruder.kraft225@gmail.com>
2020-05-19 16:09:44 -0500

    feat(interfaceparam) implement general interface param feat(peers.update) update storage feat(peers.add) add new peer feat(peers.delete) delete peer

--- ws/paramtypes.go
+++ ws/paramtypes.go
@@ -73,3 +73,69 @@ func (param *IPNetParam) UnmarshalJSON(jsonData []byte) (err error) {
 	err = json.Unmarshal(jsonData, data)
 	return
 }
+
+type InterfaceParam struct {
+	Name       string
+	Default    map[string]interface{}
+	IsRequired bool
+	data       json.RawMessage
+}
+
+func (param *InterfaceParam) Clone(data json.RawMessage) (parameters.Param, error) {
+	clone := InterfaceParam{param.Name, param.Default, param.IsRequired, param.data}
+	if data != nil {
+		err := clone.SetData(data)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if data == nil && param.IsRequired {
+		return nil, errors.New("value needed")
+	}
+	return &clone, nil
+}
+
+func (param *InterfaceParam) SetName(newName string) {
+	param.Name = newName
+}
+
+func (param *InterfaceParam) GetName() string {
+	return param.Name
+}
+
+func (param *InterfaceParam) SetData(message json.RawMessage) (err error) {
+	param.data = message
+	_, err = param.GetInterface()
+	return
+}
+
+func (param *InterfaceParam) GetData() json.RawMessage {
+	if param.data == nil {
+		data, _ := json.Marshal(param.Default)
+		return data
+	}
+	return param.data
+}
+
+func (param *InterfaceParam) GetInterface() (value map[string]interface{}, err error) {
+	err = json.Unmarshal(param.GetData(), &value)
+	return
+}
+
+func (param *InterfaceParam) MarshalJSON() ([]byte, error) {
+	if param.data == nil {
+		data, err := json.Marshal(param.Default)
+		if err != nil {
+			return nil, err
+		}
+		return data, nil
+	}
+	return param.data, nil
+}
+
+func (param *InterfaceParam) UnmarshalJSON(jsonData []byte) (err error) {
+	data := param.Default
+	err = json.Unmarshal(jsonData, data)
+	return
+}
--- ws/websocket.go
+++ ws/websocket.go
@@ -11,7 +11,7 @@ import (
 	"github.com/bob620/baka-rpc-go/rpc"
 	"github.com/gorilla/websocket"
 
-	"github.com/bob620/bakaguard/guard"
+	Guard "github.com/bob620/bakaguard/guard"
 	"github.com/bob620/bakaguard/ws/state"
 )
 
@@ -22,7 +22,7 @@ type WS struct {
 	client *rpc.BakaRpc
 }
 
-func CreateWs(guard *guard.Guard, state *state.State) WS {
+func CreateWs(guard *Guard.Guard, state *state.State) WS {
 	ws := WS{rpcClient}
 
 	rpcClient.RegisterMethod(
@@ -102,6 +102,7 @@ func CreateWs(guard *guard.Guard, state *state.State) WS {
 			&parameters.StringParam{Name: "name"},
 			&parameters.StringParam{Name: "description"},
 			&parameters.StringParam{Name: "keepAlive", Default: "-1s"},
+			&InterfaceParam{Name: "storage", Default: map[string]interface{}{}},
 			&IPNetParam{Name: "allowedIPs", Default: []net.IPNet{}},
 		},
 		func(params map[string]parameters.Param) (returnMessage json.RawMessage, err error) {
@@ -114,6 +115,7 @@ func CreateWs(guard *guard.Guard, state *state.State) WS {
 			desc, _ := params["description"].(*parameters.StringParam).GetString()
 			keepAlive, _ := params["keepAlive"].(*parameters.StringParam).GetString()
 			allowedIPs, _ := params["allowedIPs"].(*IPNetParam).GetIPNet()
+			storage, _ := params["storage"].(*InterfaceParam).GetInterface()
 
 			peer, err := guard.GetWgPeer(uuid)
 			if err != nil {
@@ -137,12 +139,79 @@ func CreateWs(guard *guard.Guard, state *state.State) WS {
 				peer.AllowedIPs = allowedIPs
 			}
 
+			if len(storage) > 0 {
+				peer.Storage = guard.FormatUpdateStorage(peer.Storage, storage)
+			}
+
 			err = guard.UpdatePeer(peer)
 			if err != nil {
 				fmt.Println(err)
 				return nil, err
 			}
 			return json.Marshal(peer)
+		})
+
+	rpcClient.RegisterMethod(
+		"peers.add",
+		[]parameters.Param{
+			&parameters.StringParam{Name: "publicKey", IsRequired: true},
+			&parameters.StringParam{Name: "name"},
+			&parameters.StringParam{Name: "description"},
+			&parameters.StringParam{Name: "keepAlive", Default: "-1s"},
+			&InterfaceParam{Name: "storage", Default: map[string]interface{}{}},
+			&IPNetParam{Name: "allowedIPs", Default: []net.IPNet{}},
+		},
+		func(params map[string]parameters.Param) (returnMessage json.RawMessage, err error) {
+			if !state.HasAdminAuth() {
+				return nil, fmt.Errorf("please authenticate as admin")
+			}
+
+			publicKey, _ := params["publicKey"].(*parameters.StringParam).GetString()
+			name, _ := params["name"].(*parameters.StringParam).GetString()
+			desc, _ := params["description"].(*parameters.StringParam).GetString()
+			keepAlive, _ := params["keepAlive"].(*parameters.StringParam).GetString()
+			allowedIPs, _ := params["allowedIPs"].(*IPNetParam).GetIPNet()
+			storage, _ := params["storage"].(*InterfaceParam).GetInterface()
+
+			keepAliveDuration := time.Duration(0)
+
+			if keepAlive != "-1s" {
+				keepAliveDuration, _ = time.ParseDuration(keepAlive)
+			}
+
+			peer := Guard.CreatePeer(publicKey, name, desc, keepAliveDuration, allowedIPs, guard.FormatUpdateStorage(nil, storage))
+
+			err = guard.SetPeer(peer)
+			if err != nil {
+				fmt.Println(err)
+				return nil, err
+			}
+			return json.Marshal(peer)
+		})
+
+	rpcClient.RegisterMethod(
+		"peers.delete",
+		[]parameters.Param{
+			&parameters.StringParam{Name: "uuid", IsRequired: true},
+		},
+		func(params map[string]parameters.Param) (returnMessage json.RawMessage, err error) {
+			if !state.HasAdminAuth() {
+				return nil, fmt.Errorf("please authenticate as admin")
+			}
+
+			uuid, _ := params["uuid"].(*parameters.StringParam).GetString()
+
+			err = guard.DeletePeer(uuid)
+			if err != nil {
+				return nil, err
+			}
+
+			peers, err := guard.GetPeers()
+			if err != nil {
+				return nil, err
+			}
+
+			return json.Marshal(peers)
 		})
 
 	return ws
