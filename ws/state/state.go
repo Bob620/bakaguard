41cf5bd1ad3e328cd617aee4e4f7b9af6b62433b
Bob620 <bruder.kraft225@gmail.com>
2020-05-19 19:16:41 -0500

    feat(auth) use new auth model

--- ws/state/state.go
+++ ws/state/state.go
@@ -1,43 +1,74 @@
 package state
 
 import (
+	"net"
+
 	"github.com/bob620/bakaguard/config"
 )
 
 type State struct {
-	config    config.Websocket
-	authScope int
+	config     config.Websocket
+	authScopes map[string]map[string]struct{}
+	hasAdmin   bool
+}
+
+type Group struct {
+	Description string
+	Network     net.IPNet
 }
 
 func InitializeConnState(config config.Websocket) *State {
 	return &State{
-		config:    config,
-		authScope: 0,
+		config:     config,
+		authScopes: map[string]map[string]struct{}{"auth.admin": {"*": {}}, "auth.user": {"*": {}}},
+		hasAdmin:   false,
+	}
+}
+
+func (state *State) GetGroupSettings(groupName string) *Group {
+	group := state.config.Groups[groupName]
+
+	return &Group{
+		Description: group.Description,
+		Network: net.IPNet{
+			IP:   net.ParseIP(group.Network.IP),
+			Mask: net.IPv4Mask(group.Network.Mask[0], group.Network.Mask[1], group.Network.Mask[2], group.Network.Mask[3]),
+		},
 	}
 }
 
 func (state *State) HasAdminAuth() bool {
-	return state.authScope >= 2
+	return state.hasAdmin
 }
 
-func (state *State) HasUserAuth() bool {
-	return state.authScope >= 1
+func (state *State) GetScopeGroups(scope string) map[string]struct{} {
+	if state.HasAdminAuth() {
+		return map[string]struct{}{"*": {}}
+	}
+
+	groups, _ := state.authScopes[scope]
+	return groups
 }
 
 func (state *State) TryAdminPassword(password string) bool {
 	pass := state.config.AdminPassword == password
 	if pass {
-		state.authScope = 2
+		state.hasAdmin = true
 	}
 
 	return pass
 }
 
-func (state *State) TryUserPassword(password string) bool {
-	pass := state.config.UserPassword == password
-	if pass {
-		state.authScope = 1
+func (state *State) TryUserLogin(username, password string) bool {
+	user, _ := state.config.Users[username]
+	if user.Password == password {
+		for group, scopes := range user.Groups {
+			for _, scope := range scopes {
+				state.authScopes[scope][group] = struct{}{}
+			}
+		}
+		return true
 	}
 
-	return pass
+	return false
 }
